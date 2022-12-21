20b21701b0a70e25ddc7a9436a3e778e72289b07
Adnan Hussain <adnanrajput42@gmail.com>
2022-12-21 09:48:45 +0500

    crud apis for todo

--- core/urls.py
+++ core/urls.py
@@ -1,1 +1,8 @@
-urlpatterns = []
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.todos, name="todos"),
+    path('delete/<str:id>', views.delete_todo, name="delete_todo"),
+    path('update/<str:id>', views.update_todo, name="update_todo")
+]
--- core/views.py
+++ core/views.py
@@ -1,6 +1,33 @@
-from django.shortcuts import render,HttpResponse
-
+import uuid
+from django.shortcuts import redirect, render
+from .models import Task
 # Create your views here.
 
+
 def todos(request):
-    return HttpResponse('<h1>')
+    if request.method == 'GET':
+        todos = Task.objects.all().values()
+        return render(request, 'index.html', {'todos': todos})
+    if request.method == 'POST':
+        todo = request.POST["todo"]
+        task = Task.objects.create(
+            user=request.user, title=todo, desciption='')
+        task.save()
+        return redirect('/')
+
+
+def update_todo(request, id):
+    # if request.method != 'PUT':
+    #     return redirect('/')
+    todo = Task.objects.get(id=uuid.UUID(id))
+    todo.complete = not todo.complete
+    todo.save()
+    return redirect('/')
+
+
+def delete_todo(request, id):
+    if request.method != 'DELETE':
+        return redirect('/')
+    todo = Task.objects.get(id=id)
+    todo.delete()
+    return redirect('/')
