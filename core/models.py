cf003c66db5e9b2841df0a57d782554bec75cd94
Adnan Hussain <adnanrajput42@gmail.com>
2022-12-21 09:39:31 +0500

    task model

--- core/models.py
+++ core/models.py
@@ -1,3 +1,20 @@
 from django.db import models
-
+from django.contrib.auth.models import User
+import uuid
 # Create your models here.
+
+
+class Task(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4)
+    user = models.ForeignKey(
+        User, on_delete=models.CASCADE, null=True, blank=True)
+    title = models.CharField(max_length=100)
+    desciption = models.TextField()
+    complete = models.BooleanField(default=False)
+    created = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        ordering = ['complete']
